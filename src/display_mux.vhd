b19327721ea8398d4848341da8e33d2efaa36fe8
mobx <45146742+m0bx@users.noreply.github.com>
2025-04-10 12:50:07 +0200

    added variables used in mux

--- src/display_mux.vhd
+++ src/display_mux.vhd
@@ -1,1 +1,19 @@
--- a
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity display_mux is
+    port (
+        clk      : in  std_logic;
+        rst      : in  std_logic;
+        sw       : in  std_logic_vector(15 downto 0);
+        btnc     : in  std_logic;
+        btnd     : in  std_logic;
+        btnl     : in  std_logic;
+        btnr     : in  std_logic;
+        segments : out std_logic_vector(6 downto 0);
+        dp       : out std_logic;
+        anodes   : out std_logic_vector(7 downto 0);
+        buzzer   : out std_logic
+    );
+end entity display_mux;
