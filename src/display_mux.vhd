a11c470b194261d9f3b473d772a1d3e578dbdbe0
mobx <45146742+m0bx@users.noreply.github.com>
2025-04-17 11:39:43 +0200

    reset

--- src/display_mux.vhd
+++ src/display_mux.vhd
@@ -41,23 +41,21 @@ architecture behavioral of display_mux is
     signal alarm_sec, alarm_min     : integer range 0 to 59 := 0;
     signal stopwatch_ms             : integer range 0 to 9999 := 0;
     
-    -- Control signals
+    -- control signals
     signal run_clock     : std_logic := '1';
     signal run_alarm     : std_logic := '1';
     signal run_stopwatch : std_logic := '1';
     
-    -- Display signals
+    -- display signals
     signal digit_select   : integer range 0 to 7 := 0;
     signal seg_data      : std_logic_vector(6 downto 0);
     signal dp_enable     : std_logic;
     
-    -- Clock enables
+    -- clock enables
     signal en_1hz    : std_logic;
     signal en_10khz  : std_logic;
 
 begin
-
-    -- Clock enable generation
     clk_en_1hz: clock_enable
         generic map (N_PERIODS => 100_000_000)
         port map (clk => clk, rst => rst, pulse => en_1hz);
@@ -65,3 +63,22 @@ begin
     clk_en_10khz: clock_enable
         generic map (N_PERIODS => 10_000)
         port map (clk => clk, rst => rst, pulse => en_10khz);
+
+    process(clk)
+        variable btnc_prev : std_logic := '0';
+        variable btnl_prev : std_logic := '0';
+        variable btnr_prev : std_logic := '0';
+	
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                clock_sec <= 0;
+                clock_min <= 0;
+                alarm_sec <= 0;
+                alarm_min <= 0;
+                stopwatch_ms <= 0;
+                run_clock <= '1';
+                run_alarm <= '1';
+                run_stopwatch <= '1';
+            else
+                -- mody clocku
